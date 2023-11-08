3a9cc4baa0eb499ae87f83c0fb3044ad8b0c3bde
M1sterGh0sT <you@example.com>
2023-11-08 18:24:01 +0100

    Add basic

---
+++ Music/mainCore/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+
+
+
+urlpatterns = [
+    path('name/', views.index, name='index'),
+    path('index2/', views.index2, name='index2'),
+]
+
+
--- Music/mainCore/views.py
+++ Music/mainCore/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+
+def index2(request):
+    return render(request, 'pages/index2.html')
+
+
+
+
+
