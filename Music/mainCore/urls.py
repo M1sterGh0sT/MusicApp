a4870585ff6f0ae6af7db4bcd5bbf96e0bd030cf
M1sterGh0sT <you@example.com>
2023-11-09 19:33:23 +0100

    Add Static file
    Add JS and CSS

--- Music/mainCore/urls.py
+++ Music/mainCore/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 
 
+app_name = 'core'
+
 
 urlpatterns = [
-    path('name/', views.index, name='index'),
-    path('index2/', views.index2, name='index2'),
+    path('name/', views.index, name='page1'),
+    path('index2/', views.index2, name='page2'),
 ]
 
 
